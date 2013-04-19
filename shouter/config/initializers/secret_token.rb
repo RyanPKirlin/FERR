077f34041223dbf673b70163e11c0bace6489af3
Ryan Patrick Kirlin <rpkirlin@gmail.com>
2013-04-19 14:30:15 -0400

    my shouter homework

---
+++ shouter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shouter::Application.config.secret_token = 'ee815b8375624fdb0b5333266df9d72932e7b10ad7bb8f3de36cd7e51be3c67c8257f32a66e5c753d32f1f5600b174964219f1a32326fec3219fbd25544d1b87'
